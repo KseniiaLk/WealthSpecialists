b0038180436516d0684aec2bd8632fc2d3708a23
Bror Lundberg <maximillian.lundber@chasacademy.se>
2024-10-31 13:26:23 +0100

    addes account class

---
+++ Account.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WealthSpecialists
+{
+    internal class Account
+    {
+        public Account(double accountBalance, string currencyType)
+        {
+            _accountBalance = accountBalance;
+            _accountNumber = Guid.NewGuid();
+            _currencyType = currencyType;
+        }
+
+        public double _accountBalance = 0;
+        public Guid _accountNumber { get; set; }
+        public string _currencyType { get; set; }
+    }
+}
